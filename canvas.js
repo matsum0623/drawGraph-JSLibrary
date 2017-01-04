4066e86355fd9c0b4b0cefe2b8a25c407ef0c9e0
matsum0623 <matsum0623@gmail.com>
2017-01-04 21:11:12 +0900

    メインファイルの作成

---
+++ canvas.js
@@ -0,0 +1,16 @@
+/**
+ * Canvasによるグラフ描画ライブラリ
+ * ES2015の機能を使用しているため、IEでは動作しない=>IE時の考慮は未実装
+ */
+class drawGraph{
+    /**
+     * コンストラクタ
+     * @param    
+     * @param
+     */
+    constructor(width,height){
+//        alert();
+    }
+    
+    
+}
